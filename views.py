39f48df00dca4e3f732eb854782b0efd0bd8b5b8
yqcdwg <yqcdwg@163.com>
2018-09-20 12:14:27 +0800

    2222222

--- views.py
+++ views.py
@@ -1,7 +1,6 @@
 
 
 def index(request):
-<<<<<<< HEAD
 	return HttpResponse("ok")
 	
 	
@@ -9,6 +8,5 @@ def index2(request):
 	return HttpResponse("ok")
 	
 def index3(request):
-=======
->>>>>>> parent of 3782104... Revert "备注: 干了什么事情"
+
 	return HttpResponse("ok")
