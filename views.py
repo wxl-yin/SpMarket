e511ecfa5d889a429c1a543be506c198e99a2c87
yqcdwg <yqcdwg@163.com>
2018-09-20 12:14:03 +0800

    Revert "Revert "备注: 干了什么事情""
    This reverts commit 378210474212b93014b8c38dfdcdd17833508b0c.
    
    # Conflicts:
    #	views.py

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 
 
 def index(request):
+<<<<<<< HEAD
 	return HttpResponse("ok")
 	
 	
@@ -8,4 +9,6 @@ def index2(request):
 	return HttpResponse("ok")
 	
 def index3(request):
+=======
+>>>>>>> parent of 3782104... Revert "备注: 干了什么事情"
 	return HttpResponse("ok")
