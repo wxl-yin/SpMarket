6ebb2b31ce825bb210ad74cf17d765654686ad8d
yqcdwg <yqcdwg@163.com>
2018-09-22 10:04:30 +0800

    修改了session的存储引擎

--- SpMarket/SpMarket/settings.py
+++ SpMarket/SpMarket/settings.py
@@ -123,3 +123,20 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+# 缓存配置
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1", # 保存redis服务器启动
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+# 配置 session 的存储引擎, 使用 redis存储session
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
+
